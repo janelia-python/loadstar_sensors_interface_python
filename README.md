4b9ebcf61b0af928c3614b342a57bfadb95b50f8
Peter Polidoro <peter@polidoro.io>
2022-08-17 16:47:10 -0400

    Basic functionality

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<!-- README.me is generated automatically from .single-source-of-truth.org
+<!-- README.md is generated automatically from .single-source-of-truth.org
     File edits may be overwritten! -->
 
 
@@ -6,21 +6,17 @@
 
 ```text
 - Name: loadstar_sensors_interface
-- Version: 0.1.0
+- Version: 0.2.0
 - Description: Python interface to Loadstar Sensors USB devices.
 - License: BSD 3-Clause License
 - URL: https://github.com/janelia-pypi/loadstar_sensors_interface_python
 - Author: Peter Polidoro
 - Email: peter@polidoro.io
 - Copyright: 2022 Howard Hughes Medical Institute
+- Reference: [Loadstar Sensors](https://www.loadstarsensors.com/)
 - Dependencies:
   - serial_interface
 ```
-
-
-# More Information
-
-This library is an interface to [Loadstar Sensors](https://www.loadstarsensors.com/) USB devices.
 
 
 # Example Usage
@@ -28,9 +24,18 @@ This library is an interface to [Loadstar Sensors](https://www.loadstarsensors.c
 ```python
 from loadstar_sensors_interface import LoadstarSensorsInterface
 dev = LoadstarSensorsInterface() # Try to automatically detect port
-dev = LoadstarSensorsInterface(port='/dev/ttyACM0') # Linux specific port
+dev = LoadstarSensorsInterface(port='/dev/ttyUSB0') # Linux specific port
 dev = LoadstarSensorsInterface(port='/dev/tty.usbmodem262471') # Mac OS X specific port
 dev = LoadstarSensorsInterface(port='COM3') # Windows specific port
+
+dev.tare()
+sensor_value = dev.get_sensor_value()
+
+model = dev.get_model()
+id = dev.get_id()
+units = dev.get_units()
+load_capactiy = dev.get_load_capacity()
+gain = dev.get_gain()
 
 ```
 
@@ -40,12 +45,33 @@ dev = LoadstarSensorsInterface(port='COM3') # Windows specific port
 <https://github.com/janelia-pypi/python_setup>
 
 
-## Linux and Mac OS X
+## Linux
+
+
+### udev rules
+
+```sh
+sudo cp 77-loadstar-sensors.rules /etc/udev/rules.d/
+```
+
+
+### pip
 
 ```sh
 python3 -m venv ~/venvs/loadstar_sensors_interface
 source ~/venvs/loadstar_sensors_interface/bin/activate
 pip install loadstar_sensors_interface
+```
+
+
+### guix
+
+Setup guix-janelia channel:
+
+<https://github.com/guix-janelia/guix-janelia>
+
+```sh
+guix install python-loadstar-sensors-interface
 ```
 
 
@@ -58,12 +84,10 @@ pip install loadstar_sensors_interface
 ```
 
 
-## Guix
-
-Setup guix-janelia channel:
-
-<https://github.com/guix-janelia/guix-janelia>
+# Test
 
 ```sh
-guix install python-loadstar-sensors-interface
+guix shell picocom
+picocom -b 9600 -f n -y n -d 8 -p 1 -c /dev/ttyUSB0
+?
 ```
--- loadstar_sensors_interface/loadstar_sensors_interface.py
+++ loadstar_sensors_interface/loadstar_sensors_interface.py
@@ -1,4 +1,6 @@
-from serial_interface import SerialInterface
+import time
+import serial
+from serial_interface import SerialInterface, ReadError
 
 
 DEBUG = False
@@ -7,10 +9,110 @@ class LoadstarSensorsInterface():
     '''
     Python interface to Loadstar Sensors USB devices.
     '''
+    _REQUEST_EOL = '\r'
+    _RESPONSE_EOL = b'\r\n'
+    _READ_ATTEMPTS = 100
+    _DURATION_BETWEEN_ATTEMPTS = 0.01
+    _GOOD_RESPONSE = b'A'
+    _WINDOW_MIN = 1
+    _WINDOW_MAX = 1024
+
     def __init__(self,*args,**kwargs):
         if 'debug' in kwargs:
             self.debug = kwargs['debug']
         else:
             kwargs.update({'debug': DEBUG})
             self.debug = DEBUG
+        kwargs.update({'baudrate': 9600,
+                       'bytesize': serial.EIGHTBITS,
+                       'parity': serial.PARITY_NONE,
+                       'stopbits': serial.STOPBITS_ONE,
+                       'xonxoff': False,
+                       'rtscts': False,
+                       'dsrdtr': False
+                       })
         self._serial_interface = SerialInterface(*args,**kwargs)
+
+    def tare(self):
+        for x in range(self._READ_ATTEMPTS):
+            response = self._write_read('tare')
+            if response == self._GOOD_RESPONSE:
+                return True
+        return False
+
+    def get_sensor_value(self):
+        for x in range(self._READ_ATTEMPTS):
+            try:
+                response = self._write_read('w')
+                sensor_value = float(response)
+                return sensor_value
+            except ValueError:
+                self._sleep()
+        return None
+
+    def get_model(self):
+        response = self._write_read('model')
+        return response
+
+    def get_id(self):
+        response = self._write_read('id')
+        return response
+
+    def get_units(self):
+        response = self._write_read('unit')
+        return response
+
+    def get_load_capacity(self):
+        response = self._write_read('lc')
+        return response
+
+    def get_gain(self):
+        response = self._write_read('gain')
+        return response
+
+    def get_settings(self):
+        response = self._write_read('settings',use_readline=False)
+        settings = response.split(self._RESPONSE_EOL)
+        return settings
+
+    def get_window(self):
+        response = self._write_read('CSS' + self._REQUEST_EOL)
+        return response
+
+    def set_window(self, count):
+        if count < self._WINDOW_MIN:
+            count = self._WINDOW_MIN
+        if count > self._WINDOW_MAX:
+            count = self._WINDOW_MAX
+        for x in range(self._READ_ATTEMPTS):
+            response = self._write_read('CSS ' + str(count))
+            if response == self._GOOD_RESPONSE:
+                return True
+        return False
+
+    def _write_read(self,request,use_readline=True):
+        for x in range(self._READ_ATTEMPTS):
+            try:
+                response = self._serial_interface.write_read(request + self._REQUEST_EOL,use_readline=use_readline)
+                if response:
+                    return response.strip()
+            except ReadError:
+                self._sleep()
+        return None
+
+    def _sleep(self):
+        time.sleep(self._DURATION_BETWEEN_ATTEMPTS)
+
+    def _test_response(self):
+        response = self._write_read('',use_readline=False)
+        return response
+
+    def _clear(self):
+        for x in range(self._READ_ATTEMPTS):
+            response = self._test_response()
+            if response.strip() == self._GOOD_RESPONSE:
+                return True
+            else:
+                self._sleep()
+        return False
+
