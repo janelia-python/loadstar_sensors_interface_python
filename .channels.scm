e8d069d4495ec846ba94252ac500363a0c8133a2
Peter Polidoro <peter@polidoro.io>
2022-08-24 14:40:55 -0400

    Reorganize directory structure

--- .channels.scm
+++ .channels.scm
@@ -3,7 +3,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "005b088fa49cc2937bcca3d64de752a34e2ccd74")
+          "6199ee19ff84f904972fcc703442dff24018ef4d")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -14,4 +14,4 @@
         (url "https://github.com/guix-janelia/guix-janelia.git")
         (branch "main")
         (commit
-          "5bb5a74d174113ebfbeef5499320b44792b46abb")))
+         "64bffa2ad80d16bab58f430e1e43ac4fa775e5bc")))
---
+++ DEVELOPMENT.md
@@ -0,0 +1,8 @@
+```shell
+git clone https://github.com/janelia-pypi/loadstar_sensors_interface_python
+cd loadstar_sensors_interface_python
+make shell
+make edits
+make all
+exit
+```
