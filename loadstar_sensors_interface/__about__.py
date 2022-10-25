4fe25c66079d44d883051379c43ebfcf9b0a11a3
Peter Polidoro <peter@polidoro.io>
2022-10-25 14:50:37 -0400

    Progress towards new metadata

--- loadstar_sensors_interface/__about__.py
+++ loadstar_sensors_interface/__about__.py
@@ -2,8 +2,8 @@
 # File edits may be overwritten!
 __version__ = '0.9.0'
 __description__ = 'Python interface to Loadstar Sensors USB devices.'
-__license__ = 'BSD 3-Clause License'
+__license__ = 'BSD-3-Clause'
 __url__ = 'https://github.com/janelia-pypi/loadstar_sensors_interface_python'
 __author__ = 'Peter Polidoro'
 __email__ = 'peter@polidoro.io'
-__copyright__ = '2022 Howard Hughes Medical Institute'
+__copyright__ = ''
